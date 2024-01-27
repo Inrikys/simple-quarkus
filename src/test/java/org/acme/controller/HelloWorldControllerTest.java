7d89f589c3f3c23683c477c3618cd149b191b641
Inrikys <inrikys@gmail.com>
2024-01-26 21:22:24 -0300

    :bulb: testando workflow

--- src/test/java/org/acme/controller/HelloWorldControllerTest.java
+++ src/test/java/org/acme/controller/HelloWorldControllerTest.java
@@ -1,19 +1,18 @@
 package org.acme.controller;
 
-import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
-@TestHTTPEndpoint(HelloWorldController.class)
 class HelloWorldControllerTest {
 
     @Test
     public void testHelloEndpoint() {
-        when().get()
+        given()
+                .when().get("/hello")
                 .then()
                 .statusCode(200)
                 .body(is("Olá! Eu e a Jessica estamos estudando"));
